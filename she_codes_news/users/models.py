03878bfb9afb741b2115699bdf46fea5f6e77e72
ashleightuvera <147504856+ashleightuvera@users.noreply.github.com>
2023-12-11 20:53:45 +0800

    Add files via upload

---
+++ she_codes_news/users/models.py
@@ -0,0 +1,18 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+#     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+#     bio = models.TextField(blank=True)
+
+#     def __str__(self):
+#         return self.user.username
+
